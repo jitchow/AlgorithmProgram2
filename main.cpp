de3b081458e7f773bb75f397397da334526f7c27
Wong Jit Chow <jcwong614@gmail.com>
2020-12-19 03:31:59 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main () {
     //This is the start of Program 2 from Algorithm Design assignment
     cout << "Hello world" << endl;
+	cout << "test branch" << endl;
     return 0;
 }
